ab794881d017bf53e5d0b7d85c8e0dd6303c3452
Gabriel Andrade <gabrielcandrade01@gmail.com>
2018-10-23 19:14:13 -0300

    Desafio comentado

--- desafio-implementado.py
+++ desafio-implementado.py
@@ -1,3 +1,8 @@
+#Autor: Gabriel de Carvalho Andrade
+#Empresa: Intelie
+
+# Argumento de entrada de dados (dado na questao)
+
 facts = [
   ('gabriel', 'endereço', 'av rio branco, 109', True),
   ('joão', 'endereço', 'rua alice, 10', True),
@@ -14,27 +19,27 @@ schemas = [
     ('telefone', 'cardinality', 'many')
 ]
 
+# Validacao de atributos ao schema, caso tenha algum atributo de facts que
+#nao corresponde ao que schemas, ele avisa que existe atributos incompativeis.
+
 def validacaoAtributos(facts, schemas):
         i = 0
-        for fact in facts:
-                for schema in schemas: 
-                        if schema[0] != fact[1]:
+        for fact in facts:      # Percorre toda a lista de facts
+                for schema in schemas:          # Percorrendo tambem a lista de schemas 
+                        if schema[0] != fact[1]:        # Quando encontra algum atributo no schema, ele soma +1
                                 i+=1
-        if i == len(facts):
+        if i == len(facts):                             # Se a quantidade de i for igual ao tamanho de facts, todos os facts foram encontrados.
                 print("Iguais")
         else:
-                print("Algum atributo diferente do schema")
-
-def exclusaoDosInativos(facts, schemas):
-        for fact in facts:
-                if fact[-1] == False:
-                        facts.remove(fact)
+                print("Algum atributo diferente do schema")     # Caso contrário, ele possui divergência
 
 def sobrescreverValor(facts, schemas):
-        factsAux = facts
-        for fact in facts:
-                for factAux in factsAux:
-                        if fact[0] == factAux[0]:
+        # Apaga os atributos que forem invalidados através da flag "False"
+        
+        factsAux = facts        # Cria uma copia de facts, porque ele não pode percorrer a própria lista, decidi percorrer uma igual.
+        for fact in facts:      # Percorre toda a lista de facts
+                for factAux in factsAux:        # Percorre toda a lista de facts copiada
+                        if fact[0] == factAux[0]:       # Aqui ele fará a validacao se tiver algum False e um True, ele apaga as duas informacoes de ambas as listas.
                                 if fact[1] == factAux[1]:
                                         if fact[2] == factAux[2]:
                                                 if fact[3] != factAux[3]:
@@ -46,24 +51,28 @@ def sobrescreverValor(facts, schemas):
                                                                 facts.remove(fact)
                                                         if factAux[3] == False:
                                                                 factsAux.remove(factAux)
-
-        for fact in facts:
-                for factAux in factsAux:
-                        if fact[0] == factAux[0]:
+        # Sobrescreve os parâmetros
+        
+        for fact in facts:      # Percorre toda a lista de facts
+                for factAux in factsAux:        # Percorre toda a lista de facts
+                        if fact[0] == factAux[0]:       # Aqui ele vai procurar conteudos diferentes de atribuitos iguais para fazer a sobreposicao. Caso positivo, ele apaga o anterior.
                                 if fact[1] == factAux[1]:
                                         if fact[2] != factAux[2]:
                                                 if fact[3] == factAux[3]:
                                                         for schema in schemas:
                                                                 if schema[2] == 'one' and fact[1] == schema[0]:
                                                                         facts.remove(fact)
-
-        for fact in facts:
-                for factAux in factsAux:
-                        for schema in schemas:
+                                                                        
+        # Percorre todos os itens e mostra apenas os conteudos que sao iguais para ambas.
+        
+        for fact in facts: # Percorre toda a lista de facts
+                for factAux in factsAux:        # Percorre toda a lista de facts
+                        for schema in schemas:  # Valida novamente todos os facts nas schemas dadas para imprimir a quantidade correta.
                                 if fact == factAux:
                                         if fact[1] == schema[0]:
-                                                print (fact)
+                                                print (fact)    # Imprime na tela os dados da lista.
         
 
 validacaoAtributos(facts, schemas)
 sobrescreverValor(facts, schemas)
+input()
